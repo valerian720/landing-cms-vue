e116b68477ee246f1a475c1622a201ad235de9b0
Валериан Воротников <valerian7202@gmail.com>
2026-05-05 22:29:26 +0300

    bootstrap dependence

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-const app = createApp(App)
+import App from "./App.vue";
+import router from "./router";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
