a7c1d6e08d9dacb633f866029b0d4308f6abd5a4
Валериан Воротников <valerian7202@gmail.com>
2026-05-30 11:06:51 +0300

    settings wip

--- src/components/SettingsPanelBlock.vue
+++ src/components/SettingsPanelBlock.vue
@@ -6,16 +6,41 @@
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <p>qwe</p>
+      <h2>Настройки магазина</h2>
+      <p>TODO</p>
+      <h2>Настройки ссылок</h2>
+      <p>TODO</p>
+      <h2>Настройки карусели</h2>
+      <p>TODO</p>
+      <h2>Настройки преимуществ</h2>
+      <p>TODO</p>
+      <h2>Настройки товаров</h2>
+      <p>TODO</p>
+      <h2>Настройки блога</h2>
+      <p>TODO</p>
+      <h2>Настройки отзывов</h2>
+      <p>TODO</p>
+      <button type="button" name="save-btn" id="save-btn" class="btn btn-apple">
+        Сохранить
+      </button>
+
+      <p>raw data in store:</p>
+      <p>{{ pageStore }}</p>
     </div>
   </div>
 </template>
 
 
 <script>
+import { usePageStore } from '@/stores/page'
+
+
 export default {
 
   props: {
-  }
+  },
+  data() {
+    return { pageStore: usePageStore() }
+  },
 }
 </script>
